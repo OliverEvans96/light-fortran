d05488258597a3a46953fc2a450988d48810536c
Oliver Evans <oliverevans96@gmail.com>
2017-01-05 15:08:33 -0500

    Remove unnecessary files

--- test/test_interp.f90
+++
@@ -1,8 +0,0 @@
-! File Name: test_interp.f90
-! Description: Test interp.f90
-! Created: Wed Jan 04, 2017 | 06:47pm EST
-! Last Modified: Thu Jan 05, 2017 | 09:24am EST
-! Author: Oliver Evans <oge1@zips.uakron.edu>
-
-program test_interp
-    use rte_utils
